e23f851e076bd6a30e81fdcb382d0f4f36ca9e45
s4 <130009819+sillycamlocker@users.noreply.github.com>
2023-05-21 18:48:18 -0700

    Create table.lua

---
+++ table.lua
@@ -0,0 +1,27 @@
+getgenv().yuki = {
+    ToggleKey = Enum.KeyCode.YourKey,
+    AimPart = {"HumanoidRootPart"},
+    Prediction = 0.119,
+    FOV = {
+        ShowFOV = true,
+        Color = Color3.fromRGB(255, 255, 255),
+        Radius = 100,
+    },
+    Smoothness = true,
+    SmoothnessValue = 0.100,
+    Shake = true,
+    ShakeValue = 35,
+    UnlockOnPlayerDeath = true,
+    UnlockOnTargetDeath = true,
+    UnlockWhenTyping = true,
+    Shake = {
+        Enabled = true,
+        X = 7,
+        Y = 4,
+        Z = 3,
+        EasingStyle = {
+            First = Enum.EasingStyle.Quint,
+            Second = Enum.EasingStyle.Exponential,
+        },
+    },
+}
